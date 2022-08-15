442ed3286cfccad692a36cc8164f989798084baf
otadjiba <oleg.tad.f@gmail.com>
2022-08-15 22:07:42 +0300

    Task 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Number a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Number b:");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if(a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+Console.WriteLine("max = " + max);
