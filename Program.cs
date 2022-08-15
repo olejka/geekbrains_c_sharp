b53c34316cca3ce0f2c96621c2394bc04e8a3da6
otadjiba <oleg.tad.f@gmail.com>
2022-08-15 22:54:20 +0300

    Task 4

--- Program.cs
+++ Program.cs
@@ -1,27 +1,7 @@
-﻿Console.WriteLine("Number a:");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Number b:");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Number c:");
-int c = Convert.ToInt32(Console.ReadLine());
-int max = 0;
-if(a > b)
-{
-    if(a > c)
-    {
-        max = a;
-    }
-    else
-    {
-        max = c;
-    }
-}
-else if(b > c)
-{
-    max = b;
-}
-else
-{
-    max = c;
-}
-Console.WriteLine("max = " + max);
+﻿Console.WriteLine("pls enter a number:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int x = 2; x < n+1; x++)       
+if (x%2==0)            
+Console.Write(x + " ");
+
