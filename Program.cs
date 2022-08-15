94753c87bccfa815e5c47dfd9d2ecf2ee5c7f37a
otadjiba <oleg.tad.f@gmail.com>
2022-08-15 22:20:40 +0300

    Task 4 (all files)

--- Program.cs
+++ Program.cs
@@ -2,13 +2,26 @@
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Number b:");
 int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Number c:");
+int c = Convert.ToInt32(Console.ReadLine());
 int max = 0;
 if(a > b)
 {
-    max = a;
+    if(a > c)
+    {
+        max = a;
+    }
+    else
+    {
+        max = c;
+    }
+}
+else if(b > c)
+{
+    max = b;
 }
 else
 {
-    max = b;
+    max = c;
 }
 Console.WriteLine("max = " + max);
