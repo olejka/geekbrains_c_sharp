12cf8fd577bde5de6e3449fb55ea936e3866c845
otadjiba <oleg.tad.f@gmail.com>
2022-08-15 22:03:44 +0300

    Очистка мастера, коммит дз будет из другого бранча

--- Program.cs
+++ Program.cs
@@ -1,14 +1,1 @@
-﻿Console.WriteLine("Number a:");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Number b:");
-int b = Convert.ToInt32(Console.ReadLine());
-int max = 0;
-if(a > b)
-{
-    max = a;
-}
-else
-{
-    max = b;
-}
-Console.WriteLine("max = " + max);
+﻿
