9230b3e4a41d54fe571725c6296168e290662ab8
otadjiba <oleg.tad.f@gmail.com>
2022-08-20 13:26:56 +0300

    Task 10

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.WriteLine("Enter a three-digit number.\nTo exit the program, click Exit.");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            //Console.WriteLine("{0}->{1}",input, input[1]);
+            Console.WriteLine("Second number: {1}",input, input[1]);
+        else
+            break;
+    }
