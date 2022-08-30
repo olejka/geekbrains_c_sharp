d735035c9db47095283562b904a18739ee40c4f8
otadjiba <oleg.tad.f@gmail.com>
2022-08-30 23:13:26 +0300

    task 21

--- Program.cs
+++ Program.cs
@@ -1,35 +1,24 @@
-﻿using System;
- 
-namespace Palindrom
+﻿int x1 = Coordinate("x", "A");
+int y1 = Coordinate("y", "A");
+int z1 = Coordinate("z", "A");
+int x2 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z2 = Coordinate("z", "B");
+
+int Coordinate(string coorName, string pointName)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int number = 0;
-            while (true)
-            {
-                Console.Write("Введите число: ");
-                if (int.TryParse(Console.ReadLine(), out number)) 
-                    break;
-                else
-                    Console.WriteLine("Неверное число.");
-            }
-            int oldValue = number;
-            int newValue = 0;
-            while (number > 0)
-            {
-                int dig = number % 10;
-                newValue = newValue * 10 + dig;
-                number = number / 10;
-            }
-            if (newValue == oldValue)
-                Console.WriteLine("Да");
-            else
-                Console.WriteLine("Нет");
-            Console.Write("Нажмите любую клавишу...");
-            Console.ReadKey();
-        }
-    }
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt16(Console.ReadLine());
 }
 
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2){
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                   Math.Pow((y2-y1), 2) + 
+                   Math.Pow((z2-z1), 2));
+}
+
+double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Длина отрезка  {segmentLength}");
