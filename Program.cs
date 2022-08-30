59182cc38c1f9886f7ffdc8ca77882cb9708318d
otadjiba <oleg.tad.f@gmail.com>
2022-08-30 23:05:26 +0300

    fix task  1

--- Program.cs
+++ Program.cs
@@ -1,14 +1,4 @@
-﻿// Console.WriteLine("Enter a three-digit number.:");
-// int n = Convert.ToInt32(Console.ReadLine());
-// if(n>99)
-//     {
-//         Console.WriteLine(n.ToString()[2]);
-//     }
-//     else
-//     {
-//         Console.WriteLine("no third digit");
-//     }
-using System;
+﻿using System;
  
 namespace Palindrom
 {
