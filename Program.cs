e6ae32570ed89ec01cbaeb2a65cd4c0c7fed6a96
otadjiba <oleg.tad.f@gmail.com>
2022-08-31 23:27:26 +0300

    task 50

--- Program.cs
+++ Program.cs
@@ -1,38 +1,34 @@
-﻿Console.Write("Введите m: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите n: ");
-int n = Convert.ToInt32(Console.ReadLine());
-
-Console.Clear();
-Console.WriteLine($"m = {m}, n = {n}.");
-
-double[,] array = new double[m, n];
-
-CreateArrayDouble(array);
-
-WriteArray(array);
-
-Console.WriteLine();
-
-void CreateArrayDouble(double[,] array)
+﻿internal class Program
 {
-  for (int i = 0; i < m; i++)
-  {
-    for (int j = 0; j < n; j++)
+    private static void Main(string[] args)
     {
-      array[i, j] = new Random().NextDouble() * 20 - 10;
-    }
-  }
-}
+        Console.WriteLine("Введите размеры массива");
+        int m = Convert.ToInt32(Console.ReadLine());
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[,] array = new int[m, n];
 
-void WriteArray (double[,] array){
-for (int i = 0; i < m; i++)
-  {
-      for (int j = 0; j < n; j++)
-      {
-        double alignNumber = Math.Round(array[i, j], 1);
-        Console.Write(alignNumber + " ");
-      }
-      Console.WriteLine();
-  }
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+                array[i, j] = Convert.ToInt32(new Random().Next(0, 21));
+        }
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+                Console.Write(array[i, j] + "\t  ");
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Введите координаты");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        if (a > m && b > n)
+            Console.WriteLine("такого числа нет");
+        else
+        {
+            object c = array.GetValue(a, b);
+            Console.WriteLine(c);
+        }
+    }
 }
