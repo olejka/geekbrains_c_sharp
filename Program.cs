f74de708f39d6fea3190f5b102266402a359b61a
otadjiba <oleg.tad.f@gmail.com>
2022-08-31 23:49:11 +0300

    task 52

--- Program.cs
+++ Program.cs
@@ -1,34 +1,67 @@
-﻿internal class Program
+﻿Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+Console.WriteLine($"m = {m}, n = {n}.");
+
+double[,] array = new double[m, n];
+
+CreateArrayDouble(array);
+
+
+Console.WriteLine();
+
+void CreateArrayDouble(double[,] array)
 {
-    private static void Main(string[] args)
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
     {
-        Console.WriteLine("Введите размеры массива");
-        int m = Convert.ToInt32(Console.ReadLine());
-        int n = Convert.ToInt32(Console.ReadLine());
-        int[,] array = new int[m, n];
-
-        for (int i = 0; i < array.GetLength(0); i++)
-        {
-            for (int j = 0; j < array.GetLength(1); j++)
-                array[i, j] = Convert.ToInt32(new Random().Next(0, 21));
-        }
-
-        for (int i = 0; i < array.GetLength(0); i++)
-        {
-            for (int j = 0; j < array.GetLength(1); j++)
-                Console.Write(array[i, j] + "\t  ");
-            Console.WriteLine();
-        }
-
-        Console.WriteLine("Введите координаты");
-        int a = Convert.ToInt32(Console.ReadLine());
-        int b = Convert.ToInt32(Console.ReadLine());
-        if (a > m && b > n)
-            Console.WriteLine("такого числа нет");
-        else
-        {
-            object c = array.GetValue(a, b);
-            Console.WriteLine(c);
-        }
+      array[i, j] = new Random().NextDouble() * 20 - 10;
     }
+  }
+}
+
+
+int[,] arrayWhole = new int[m, n];
+arrayWhole = TransformationArrayWhole(array);
+
+WriteArrayInt(arrayWhole);
+
+Console.Write($"\nCреднее арифметическое:\n");
+for (int i = 0; i < n; i++)
+{
+  double arithmeticMean = 0;
+  for (int j = 0; j < m; j++)
+  {
+    arithmeticMean += arrayWhole[j, i];
+  }
+  arithmeticMean = Math.Round(arithmeticMean / m, 1);
+  Console.WriteLine($"столбца № {i+1} {arithmeticMean}");
+}
+
+int[,] TransformationArrayWhole (double[,] array)
+{
+  int[,] arrayWhole = new int[array.GetLength(0), array.GetLength(1)];
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      arrayWhole[i, j] = Convert.ToInt32(array[i, j]);
+    }
+  }
+  return arrayWhole;
+}
+
+void WriteArrayInt (int[,] arrayWhole){
+for (int i = 0; i < m; i++)
+  {
+      for (int j = 0; j < n; j++)
+      {
+        Console.Write(arrayWhole[i, j] + " ");
+      }
+      Console.WriteLine();
+  }
 }
