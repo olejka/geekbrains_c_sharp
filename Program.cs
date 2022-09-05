70ad48fa4e4860c654df7a83113304e3d66a23a2
otadjiba <oleg.tad.f@gmail.com>
2022-09-05 23:43:46 +0300

    task 68

--- Program.cs
+++ Program.cs
@@ -1,25 +1,16 @@
-﻿//Console.Clear();
+﻿Console.Clear();
 int m = InputNumbers("Введите m: ");
 int n = InputNumbers("Введите n: ");
-int temp = m;
 
-if (m > n) 
+int functionAkkerman = Ack(m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Ack(int m, int n)
 {
-  m = n; 
-  n = temp;
-}
-
-PrintSumm(m, n, temp=0);
-
-void PrintSumm(int m, int n, int summ)
-{
-  summ = summ + n;
-  if (n <= m)
-  {
-    Console.Write($"Сумма элементов= {summ} ");
-    return;
-  }
-  PrintSumm(m, n - 1, summ);
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
 }
 
 int InputNumbers(string input) 
