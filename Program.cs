570ce59edb3ddc91e59f6ffdd6716a2e54f0570d
otadjiba <oleg.tad.f@gmail.com>
2022-09-05 23:24:11 +0300

    task 64

--- Program.cs
+++ Program.cs
@@ -1,67 +1,19 @@
-﻿Console.Write("Введите m: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите n: ");
-int n = Convert.ToInt32(Console.ReadLine());
+﻿Console.Clear();
+int n = InputNumbers("Введите n: ");
+int count = 2;
+PrintNumber(n, count);
+Console.Write(1);
 
-Console.Clear();
-Console.WriteLine($"m = {m}, n = {n}.");
-
-double[,] array = new double[m, n];
-
-CreateArrayDouble(array);
-
-
-Console.WriteLine();
-
-void CreateArrayDouble(double[,] array)
+void PrintNumber(int n, int count)
 {
-  for (int i = 0; i < m; i++)
-  {
-    for (int j = 0; j < n; j++)
-    {
-      array[i, j] = new Random().NextDouble() * 20 - 10;
-    }
-  }
+  if (count > n) return;
+  PrintNumber(n, count + 1);
+  Console.Write(count + ", ");
 }
 
-
-int[,] arrayWhole = new int[m, n];
-arrayWhole = TransformationArrayWhole(array);
-
-WriteArrayInt(arrayWhole);
-
-Console.Write($"\nCреднее арифметическое:\n");
-for (int i = 0; i < n; i++)
+int InputNumbers(string input) 
 {
-  double arithmeticMean = 0;
-  for (int j = 0; j < m; j++)
-  {
-    arithmeticMean += arrayWhole[j, i];
-  }
-  arithmeticMean = Math.Round(arithmeticMean / m, 1);
-  Console.WriteLine($"столбца № {i+1} {arithmeticMean}");
-}
-
-int[,] TransformationArrayWhole (double[,] array)
-{
-  int[,] arrayWhole = new int[array.GetLength(0), array.GetLength(1)];
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      arrayWhole[i, j] = Convert.ToInt32(array[i, j]);
-    }
-  }
-  return arrayWhole;
-}
-
-void WriteArrayInt (int[,] arrayWhole){
-for (int i = 0; i < m; i++)
-  {
-      for (int j = 0; j < n; j++)
-      {
-        Console.Write(arrayWhole[i, j] + " ");
-      }
-      Console.WriteLine();
-  }
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
 }
